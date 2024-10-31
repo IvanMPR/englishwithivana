50008362f6126e5473260a09d2687e8a5edb1360
IvanMPR <ivsanmas@gmail.com>
2024-10-31 20:05:44 +0100

    restore Header component in Home page and remove from RootLayout

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,5 +1,6 @@
 import TranslationsProvider from "../_components/TranslationsProvider";
 import initTranslations from "../i18n";
+import Header from "../_components/Header";
 
 export default async function Home({ params: { locale } }) {
   const { t, resources } = await initTranslations(locale, ["home"]);
@@ -12,6 +13,7 @@ export default async function Home({ params: { locale } }) {
       <div>
         <h1>{t("header")}</h1>
         <p>{t("content")}</p>
+        <Header />
       </div>
     </TranslationsProvider>
   );
