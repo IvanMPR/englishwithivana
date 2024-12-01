2008bc28362843e3880c5d1a7e4f207a783b0f67
IvanMPR <ivsanmas@gmail.com>
2024-12-01 21:58:15 +0100

    remove about page layout component; update page content and structure for clarity

--- app/[locale]/about-ivana/page.js
+++ app/[locale]/about-ivana/page.js
@@ -4,8 +4,10 @@ export default async function Page({ params: { locale } }) {
   const { t } = await initTranslations(locale, ["about"]);
   return (
     <div>
-      <h1>{t("header")}</h1>
+      <p>Hello, this is about page</p>
       <p>{t("content")}</p>
+      {/* <h1>{t("header")}</h1>
+      <p>{t("content")}</p> */}
     </div>
   );
 }
