f615f703a1baed7746ed5437cd0f7671096447c1
IvanMPR <ivsanmas@gmail.com>
2024-12-25 22:35:04 +0100

    refactor Hero component layout; enhance text structure and add call-to-action button for improved user engagement

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -8,16 +8,28 @@ export default function Hero({ backgroundImage }) {
       className='relative bg-cover bg-center h-[35rem] bg-no-repeat clip-polygon'
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className='absolute inset-0 bg-gradient-to-b from-darkColor to-filterColor opacity-80'></div>
-      <div className=''>
-        <h1 className='absolute inset-0 flex flex-col items-center justify-center text-white text-4xl font-bold'>
-          <span className='uppercase text-sm font-normal '>Ivana Miceta</span>{" "}
-          Online Classes and Tranlsating & Interpreting Services
-          <span className='uppercase text-sm font-normal'>
-            Online classes of English, Spanish and Serbian language and
-            Translation and Interpreting services
-          </span>
-        </h1>
+      <div className='absolute inset-0 bg-gradient-to-b from-darkColor to-filterColor opacity-80'>
+        {" "}
+        <div className=' absolute w-1/2 m-auto inset-0 flex flex-col items-center justify-center '>
+          <h1 className=' text-white text-4xl font-bold px-5 leading-tight'>
+            <span className='block uppercase text-sm text-primaryColor mb-5'>
+              Ivana Miceta
+            </span>{" "}
+            Online Classes and Translation & Interpreting Services
+            <span className='block text-sm font-normal mt-5'>
+              Online classes of English, Spanish and Serbian language and
+              Translation and Interpretation services.
+            </span>
+          </h1>
+          <div className='w-full mt-10 px-5 flex items-start '>
+            <button className=' text-sm rounded text-white bg-primaryColor px-3 py-[.5em]'>
+              Check all my classes
+            </button>
+            <div className='ml-8 pt-1'>
+              <p className=' text-white'>&#x2794; Why me?</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
