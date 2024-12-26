abdbf3e05847abd40afe56a870ada70fdce2ac75
IvanMPR <ivsanmas@gmail.com>
2024-12-26 17:12:11 +0100

    implement i18n support in Hero component; update text content and button labels for English and Spanish languages

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -1,8 +1,10 @@
-// import heroImage from "./public/home_bgd.jpg";
-// import Hamburger from "hamburger-react";
-import Header from "../_components/Header";
+"use client";
+
+import { useTranslation } from "react-i18next";
 
 export default function Hero({ backgroundImage }) {
+  const { t } = useTranslation(["home", "common"]);
+
   return (
     <div
       className='relative bg-cover bg-center h-[35rem] bg-no-repeat clip-polygon'
@@ -13,20 +15,21 @@ export default function Hero({ backgroundImage }) {
         <div className=' absolute w-1/2 m-auto inset-0 flex flex-col items-center justify-center '>
           <h1 className=' text-white text-4xl font-bold px-5 leading-tight'>
             <span className='block uppercase text-sm text-primaryColor mb-5'>
-              Ivana Miceta
+              {t("ivana miceta", { ns: "common" })}
             </span>{" "}
-            Online Classes and Translation & Interpreting Services
+            {t("h1", { ns: "home" })}
             <span className='block text-sm font-normal mt-5'>
-              Online classes of English, Spanish and Serbian language and
-              Translation and Interpretation services.
+              {t("h1_extended", { ns: "home" })}
             </span>
           </h1>
           <div className='w-full mt-10 px-5 flex items-start '>
             <button className=' text-sm rounded text-white bg-primaryColor px-3 py-[.5em]'>
-              Check all my classes
+              {t("button_text", { ns: "home" })}
             </button>
             <div className='ml-8 pt-1'>
-              <p className=' text-white'>&#x2794; Why me?</p>
+              <p className=' text-white'>
+                &#x2794; {t("button_why", { ns: "home" })}
+              </p>
             </div>
           </div>
         </div>
