45dbed11214df8765c91aca41d56048c5e219965
IvanMPR <ivsanmas@gmail.com>
2025-01-30 15:08:16 +0100

    implement TranslationsProvider and Hero component in InterpretingTranslations page; initialize translations and set background image

--- app/[locale]/interpreting-translations/page.js
+++ app/[locale]/interpreting-translations/page.js
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <p>Welcome to my translations page</p>;
+// import TranslationsProvider from "../_components/TranslationsProvider";
+import TranslationsProvider from "../../_components/TranslationsProvider";
+import initTranslations from "../../i18n";
+import Hero from "../../_components/Hero";
+
+export default async function InterpretingTranslations({ params: { locale } }) {
+  const { t, resources } = await initTranslations(locale, [
+    "interpreting-translations",
+  ]);
+  return (
+    <TranslationsProvider
+      resources={resources}
+      locale={locale}
+      namespaces={["interpreting-translations"]}
+    >
+      <Hero backgroundImage='/interpreting-bg-image.jpg' />
+    </TranslationsProvider>
+  );
 }
